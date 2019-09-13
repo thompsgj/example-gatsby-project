32b82abff8a51aabdd30120672c87f71f5205f7b
Greg <thompsgj@yahoo.com>
2019-09-13 10:15:27 +0900

    Fix Typo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import styles from "../css/error.module.css"
 import Banner from "../components/banner"
 import { Link } from "gatsby"
