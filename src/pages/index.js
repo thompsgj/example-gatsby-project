bf3a66c78d83411f3e577e55712a7b47df4ea499
Greg <thompsgj@yahoo.com>
2019-09-25 22:56:12 +0900

    Bug Fix - Typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Banner from "../components/banner"
 import About from "../components/Home/about"
 import Services from "../components/home/services"
